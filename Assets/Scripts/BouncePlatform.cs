a55f2a42ec2c5e20b7b80d38e3183c0c93ab7fcd
hoodey <41493055+hoodey@users.noreply.github.com>
2024-05-09 20:59:43 -0500

    Edited the spawn rate and bounce heights

--- Assets/Scripts/BouncePlatform.cs
+++ Assets/Scripts/BouncePlatform.cs
@@ -6,7 +6,7 @@ public class BouncePlatform : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other){
         if(other.gameObject.CompareTag("Bounce")){
-            float bounceForce = Random.Range(5.0f, 12.0f);
+            float bounceForce = Random.Range(6.0f, 11.0f);
             GameObject myObject = other.gameObject;
             Rigidbody2D rb2D = myObject.GetComponent<Rigidbody2D>();
             rb2D.AddForce(rb2D.transform.up * bounceForce, ForceMode2D.Impulse);
