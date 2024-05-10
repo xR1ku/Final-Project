57086c933bf25d34530f87c7f9b76cc0c6b40f2d
CPK-c <157927732+CPK-c@users.noreply.github.com>
2024-05-10 12:43:52 -0500

    Platform Implemented
    Made Platform and allows player to jump on it and also respawns when colliding with death wall. Also interacts with enemies.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            transform.position = new Vector3(10.5f, -3f, 0);
+        }
+    }
+}
